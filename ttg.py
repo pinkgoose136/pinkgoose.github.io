e2ecfa3e61d5b8cbcc312e86bbdb4b8c19e101f3
Artёm <74594500+pinkgoose136@users.noreply.github.com>
2023-11-28 00:14:33 +0100

    Update ttg.py

--- ttg.py
+++ ttg.py
@@ -4,6 +4,7 @@ import json
 from adding import check_link
 from bs4 import BeautifulSoup
 from db import *
+import requests
 prov = '981556791'
 
 async def add(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -28,15 +29,15 @@ async def aie(update: Update, context: CallbackContext):
                 await update.message.reply_text(re['error'])
                 add_data(res['date'])
             else:
-                tex = f"Ссылка: {res['link']}\nОписание: {res['desc']}\nТип: {res['type']}\nКатегория: {get_el(res['cat'])}\nТеги: {get_el(res['tag'])}"
-                await update.message.reply_text(tex, reply_markup=ReplyKeyboardRemove())
-                btns = [
-                [InlineKeyboardButton("Одобрить заявку", callback_data=f"ODOBRIT True {re['link']} {update.message.chat_id}")],
-                [InlineKeyboardButton("Удалить", callback_data=f"ODOBRIT False {re['link']} {update.message.chat_id}")],
-                [InlineKeyboardButton(text="Check", web_app=WebAppInfo(url="https://pinkgoose136.github.io/pinkgoose.github.io/item.html?idi="+res['date']))]
-            ]
-                reply_markup = InlineKeyboardMarkup(btns)
-                await context.bot.send_message(chat_id=prov, text=tex, reply_markup=reply_markup)
+                response = requests.get(re['img'])
+                if response.status_code == 200:
+                    with open(f"tgex/img/img-{res['date']}.jpg", 'wb') as file:
+                        file.write(response.content)
+                await update.message.reply_text("Данные успешно получены, осталось подтвердить отправку заявки", reply_markup=ReplyKeyboardRemove())
+                await update.message.reply_text("Нажмите на кнопку ниже",
+                    reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(text="Подтвердите отправку заявки",
+                    web_app=WebAppInfo(url=f"https://pinkgoose136.github.io/pinkgoose.github.io/item.html?idi={res['date']}&name={re['tgnam']}&img={re['img']}"))))
+
         except Exception as e:
             if 'error' in res.keys():
                 await update.message.reply_text(res['error'], reply_markup=ReplyKeyboardRemove())
@@ -45,42 +46,18 @@ async def aie(update: Update, context: CallbackContext):
                 await update.message.reply_text(str(e), reply_markup=ReplyKeyboardRemove())
                 add_data(res['date'])
     elif res['name'] == 'check':
-        rer = str(res['links'])
+        rer = res['links']
+        abe = rer.keys()
+        for t in abe:
+            u = rer[t]
+            kt = ''
+            if u['answer'] == "NO": 
+                kt = ' не '
+            await context.bot.send_message( int(u['owner']), f'Ваша заявка на добавление {u["link"] }{kt} была одобрена', disable_web_page_preview=True)
+
         if rer == '':
             rer = 'Пусто'
-        await update.message.reply_text(rer, reply_markup=ReplyKeyboardRemove())
-
-async def question(update, context):
-    query = update.callback_query.data
-    q = query.split(' ')
-    if q[0] == 'ODOBRIT':
-        if q[1] == 'False':
-            #au = update.callback_query.from_user.id
-            #autr = showitem2(connectdb(), 'list', 'name', qu[2], 'author')[0][0]
-            #delt(connectdb(), query.split(' ')[2])
-            #delclt(connectdb(), query.split(' ')[2])
-
-            await context.bot.send_message(q[3], f'Ваша заявка на добавление https://t.me/{q[2]} не была одобрена', disable_web_page_preview=True)
-            chat_id = update.callback_query.message.chat_id
-            message_id = update.callback_query.message.message_id
-            await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
-
-        elif q[1] == 'True':
-            omg = []
-            bu = update.callback_query.message.text.split('\n')
-            for b in bu:
-                omg.append(b.split(': ')[1])
-            print(omg)
-            x1 = [[x, 'category'] for x in omg[3].split(',')]
-            x2 = [[x, 'tags'] for x in omg[4].split(',')]
-            print(x1+x2)
-            add_data(omg[0], omg[1], omg[2], q[3], x1+x2)
-
-            await context.bot.send_message(q[3], f'Ваша заявка на добавление https://t.me/{q[2]} была одобрена', disable_web_page_preview=True)
-            await context.bot.send_message(q[3], str(get_data(q[3])), disable_web_page_preview=True)
-            chat_id = update.callback_query.message.chat_id
-            message_id = update.callback_query.message.message_id
-            await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+        await update.message.reply_text('Готово', reply_markup=ReplyKeyboardRemove())
 
 def get_el(el):
     soup = BeautifulSoup(el, 'html.parser')
@@ -90,17 +67,30 @@ def get_el(el):
     else:
         return "Отсутствуют"
 
+async def kete(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Нажмите на кнопку ниже",
+    reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(text="Открыть каталог",
+    web_app=WebAppInfo(url="https://pinkgoose136.github.io/pinkgoose.github.io/kete.html"))))
+
+async def bad(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Нажмите на кнопку ниже",
+    reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(text="Открыть каталог",
+    web_app=WebAppInfo(url="https://pinkgoose136.github.io/pinkgoose.github.io/bad.html"))))
 
 async def list(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Нажмите на кнопку ниже",
     reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(text="Открыть каталог",
     web_app=WebAppInfo(url="https://pinkgoose136.github.io/pinkgoose.github.io/list.html"))))
 
+async def mylist(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Нажмите на кнопку ниже",
+    reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(text="Открыть каталог",
+    web_app=WebAppInfo(url="https://pinkgoose136.github.io/pinkgoose.github.io/my_items.html"))))
+
 
 async def alist(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if str(update.message.chat_id) == prov: 
         key = [[KeyboardButton(text="Нажмите на кнопку в меню", web_app=WebAppInfo(url="https://pinkgoose136.github.io/pinkgoose.github.io/alist.html"))],]
-        
         await update.message.reply_text(
             "Открыть список заявок",
             reply_markup=ReplyKeyboardMarkup(key, resize_keyboard=True),
@@ -129,8 +119,10 @@ def main() -> None:
     application.add_handler(CommandHandler("list", list))
     application.add_handler(CommandHandler("alist", alist))
     application.add_handler(CommandHandler("clear", clear))
+    application.add_handler(CommandHandler("kete", kete))
+    application.add_handler(CommandHandler("bad", bad))
+    application.add_handler(CommandHandler("mylist", mylist))
     application.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, aie))
-    application.add_handler(CallbackQueryHandler(question))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
